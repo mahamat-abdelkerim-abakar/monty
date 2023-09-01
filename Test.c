f1f584ad5b33035412546a1a4d6c14bec3a99301
Mahamat Abdelkerim Abakar <65360822+mahamat-abdelkerim-abakar@users.noreply.github.com>
2023-09-01 22:36:05 +0530

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,30 +1,37 @@
-monty
-Ei
-Igt
-Rty
-gyfm
-P5yr
-u
-d4d
-Gt
-Mmr
-Eig
-2f
-Wtt
-erdt
-pu
-ugff
-tk
-lcg
-l
-ty
-j
-fb
-gl
-t
-tr
-tt
-ui
-yk
-tu
-d
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int stack[100];
+  int top = -1;
+  char *opcode;
+  while (scanf("%s", opcode) != EOF) {
+    if (strcmp(opcode, "push") == 0) {
+      int value;
+      scanf("%d", &value);
+      stack[++top] = value;
+    } else if (strcmp(opcode, "pop") == 0) {
+      top--;
+    } else if (strcmp(opcode, "add") == 0) {
+      stack[top] += stack[top - 1];
+      top--;
+    } else if (strcmp(opcode, "sub") == 0) {
+      stack[top] -= stack[top - 1];
+      top--;
+    } else if (strcmp(opcode, "mul") == 0) {
+      stack[top] *= stack[top - 1];
+      top--;
+    } else if (strcmp(opcode, "div") == 0) {
+      stack[top] /= stack[top - 1];
+      top--;
+    } else if (strcmp(opcode, "pall") == 0) {
+      for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+      }
+      printf("\n");
+    } else {
+      printf("Unknown opcode: %s\n", opcode);
+    }
+  }
+  return 0;
+}
